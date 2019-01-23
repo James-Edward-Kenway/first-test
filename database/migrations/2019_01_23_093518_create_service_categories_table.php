3540899aab7427e9599f11e59476de6bd7c21b81
Edward <karimovjavohir.uz@gmail.com>
2019-01-23 14:50:23 +0500

    migrations and models

---
+++ database/migrations/2019_01_23_093518_create_service_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServiceCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('service_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id');
+            $table->string('name',512);
+            $table->string('image64');
+            $table->string('image128');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('service_categories');
+    }
+}
---
+++ database/migrations/2019_01_23_093530_create_product_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id');
+            $table->string('name',512);
+            $table->string('image64');
+            $table->string('image128');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+}
